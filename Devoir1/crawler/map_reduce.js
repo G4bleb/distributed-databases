71e1d929372b82eb74c1fabfba26dc8c26ca40c0
Gableb <gableb@hotmail.fr>
2020-01-30 18:48:25 -0500

    renamed files

---
+++ Devoir1/crawler/map_reduce.js
@@ -0,0 +1,25 @@
+db.spells.mapReduce(
+    function(){emit(this.name, 1);},
+    function(key, values){
+        values;
+        }
+    ,
+    {
+        query: { $and: [{ "level.sorcerer/wizard": { $lte: 4 } }, { components: ["V"] }] },
+        out: "usable_spells"
+    }
+)
+
+db.spells.mapReduce(
+    function(){emit(this.name, 1);},
+    function(key, values){
+        return Array.sum(values);
+        }
+    ,
+    {
+        query: { $and: [{ "level.sorcerer/wizard": { $lte: 4 } }, { components: ["V"] }] },
+        out: "usable_spells"
+    }
+)
+
+db.spells.find({ $and: [{"level.sorcerer/wizard": { $lte: 4 }}, {components: ["V"]}] })
