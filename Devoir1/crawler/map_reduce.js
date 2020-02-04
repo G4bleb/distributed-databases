acf33a3453346385e52bc1bd15aa6168e20077e8
Gableb <gableb@hotmail.fr>
2020-02-04 17:31:31 -0500

    cleaned some more

--- Devoir1/crawler/map_reduce.js
+++ Devoir1/crawler/map_reduce.js
@@ -10,16 +10,4 @@ db.spells.mapReduce(
     }
 )
 
-db.spells.mapReduce(
-    function () { emit(this.name, 1); },
-    function (key, values) {
-        return Array.sum(values);
-    }
-    ,
-    {
-        query: { $and: [{ "level.sorcerer/wizard": { $lte: 4 } }, { components: ["V"] }] },
-        out: "usable_spells"
-    }
-)
-
 db.spells.find({ $and: [{ "level.sorcerer/wizard": { $lte: 4 } }, { components: ["V"] }] })
