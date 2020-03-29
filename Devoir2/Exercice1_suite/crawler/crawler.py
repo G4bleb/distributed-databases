f7f9931f6e5f498dfdd436546c6c6d8b5b9e596d
Gableb <gableb@hotmail.fr>
2020-03-28 21:16:50 -0400

    devoir2 crawler basically done

--- Devoir2/Exercice1_suite/crawler/crawler.py
+++ Devoir2/Exercice1_suite/crawler/crawler.py
@@ -7,33 +7,74 @@ class DXSpider(scrapy.Spider):
 
     def __init__(self, name=None, **kwargs):
         super().__init__(name=name, **kwargs)
-        self.download_delay = 1  # 0.5
-
+        self.download_delay = 2  # 0.5
+    
     def start_requests(self):
-        ## grab the first URL to being crawling
-        # PAGE_URL_SELECTOR = '.index > ul > li > a ::attr(href)
-        base_url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID="
-        for i in range(1, 617):  # 1 to 616 : PFRPG Core
-            yield scrapy.Request(  # yield
-                base_url + str(i),
-                # response.urljoin('http://legacy.aonprd.com/bestiary/angel.html#angel-solar'),
-                callback=self.parsePage
-            )
+        with open('links/links.txt', 'r') as f:  # read the list of urls
+           for url in f.readlines():             # process each of them
+                yield scrapy.Request(  # yield
+                    url,
+                    # response.urljoin('http://legacy.aonprd.com/bestiary/angel.html#angel-solar'),
+                    callback=self.parse
+                )
+                # break
 
-    def parsePage(self, response):
-        print("PARSING : "+response.url)
+    # def start_requests(self):
+    #     ## grab the first URL to being crawling
+    #     # PAGE_URL_SELECTOR = '.index > ul > li > a ::attr(href)
+    #     base_url = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID="
+    #     for i in range(1, 617):  # 1 to 616 : PFRPG Core
+    #         yield scrapy.Request(  # yield
+    #             base_url + str(i),
+    #             callback=self.parsePage
+    #         )
 
-        # NAME_SELECTOR = '.body .stat-block-title'
-        # creaturesStatBlockTitles = response.css(NAME_SELECTOR).getall()
-        # print(creaturesStatBlockTitles)
+    def parse(self, response):
+        spell = {}
 
-        # CONTENT_SELECTOR = '.body'
-        # page_content = response.css(CONTENT_SELECTOR).get()
-        # spells = parseContent(page_content)
-        # for i, sbt in enumerate(creaturesStatBlockTitles):
-        #     try:
-        #         yield {'name': getCreatureNameFromStatBlockTitle(sbt), 'spells': spells[i]}
-        #     except IndexError:
-        #         yield {'name': getCreatureNameFromStatBlockTitle(sbt), 'spells': []}
-        #     except AssertionError:
-        #         pass
+        NAME_SELECTOR = '.heading ::text'
+        spell['name'] = response.css(NAME_SELECTOR).get()
+        
+        # INFOS_SELECTOR = '.SPDet'
+        # infos_response = response.css(INFOS_SELECTOR)
+
+        INFOS_SELECTOR = '.SPDet ::text'
+        # print(infos_content)
+        infos = response.css(INFOS_SELECTOR).getall()
+
+        for title, content in zip(infos[0::2], infos[1::2]): #syntax : (list[start:end:step])
+            spell[title.strip().lower().replace(' ', '_')] = content.strip().strip(';')
+
+        details = parseDetails(spell['level'])
+        spell['level'] = {}
+        for detail in details:
+            caster, level = detail.split(' ')
+            spell['level'][caster] = int(level)
+
+        details = parseDetails(spell['components'])
+        spell['components'] = []
+        for detail in details:
+            # component = detail.split(' ')[0]
+            spell['components'].append(detail)
+        
+        try:
+            spell['spell_resistance'] = ("yes" in spell['spell_resistance'])
+        except KeyError:
+            spell['spell_resistance'] = False
+
+        DESCRIPTION_SELECTOR = '.SPDesc ::text'
+        spell['description'] = response.css(DESCRIPTION_SELECTOR).get()
+
+        # print(spell)
+        return spell
+
+def parseInfos(infos_content):
+    print(infos_content)
+    return infos_content
+
+#Separates values by commas
+def parseDetails(desc):
+    details = desc.split(",")
+    details = [d.strip() for d in details]
+    # details = [d.replace(';', '') for d in details]
+    return details
